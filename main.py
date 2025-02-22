1b40b20d35169468d5e7b1a924ae9d1409c899c6
Risheet Lenka <risheetlenka01@gmail.com>
2025-02-21 22:32:52 -0800

    created files and installed dependencies

---
+++ main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
